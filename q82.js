1c2f3ff9c5a27a167edb17006b5d5754a2c76aad
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-28 02:22:12 +0500

    File ofq82

---
+++ q82.js
@@ -0,0 +1,7 @@
+//Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+// This function counts how many characters are in a string
+function stringLength(str) {
+    return str.length;
+}
+console.log(stringLength("Marium")); // Outputs: 6
+// We're simply asking how long the string "Alice" is, and it tells us there are 5 characters.
